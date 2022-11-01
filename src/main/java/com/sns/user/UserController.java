69303ce06e942c67b421a95d591a35276a6bc970
dkahsem27 <dkahsem27@gmail.com>
2022-11-01 21:43:38 +0900

    1101 마이페이지 데이터 뿌리기 구현중

--- src/main/java/com/sns/user/UserController.java
+++ src/main/java/com/sns/user/UserController.java
@@ -2,14 +2,21 @@ package com.sns.user;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sns.user.bo.UserBO;
+import com.sns.user.model.User;
 
 @RequestMapping("/user")
 @Controller
 public class UserController {
 
+	@Autowired
+	private UserBO userBO;
+	
 	/**
 	 * 회원가입 화면
 	 * @param model
@@ -32,6 +39,11 @@ public class UserController {
 		return "template/layout";
 	}
 	
+	/**
+	 * 로그아웃 API
+	 * @param session
+	 * @return
+	 */
 	@RequestMapping("/sign_out")
 	public String signOut(HttpSession session) {
 		session.removeAttribute("userName");
@@ -40,8 +52,18 @@ public class UserController {
 		return "redirect:/user/sign_in_view";
 	}
 	
+	
 	@RequestMapping("/my_page_view")
-	public String mypage(Model model) {
+	public String mypage(HttpSession session, Model model) {
+		Integer userId = (Integer)session.getAttribute("userId");
+		if (userId == null) {
+			// 로그인이 풀려있으면 로그인 페이지로 리다이렉트
+			return "redirect:/user/sign_in_view";
+		}
+		
+		User<User> user = userBO.;
+		model.addAttribute("user", user);
+		
 		model.addAttribute("viewName", "user/myPage");
 		return "template/layout";
 	}
